47946f907448aed6c02bd9cb024ac8aecfc70c8e
ck <cengizkandemir@live.com>
2019-04-28 15:36:31 +0300

    Fix warnings

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 int main(int argc, char** argv)
 {
+    (void)argc;
+    (void)argv;
+
     logger::filter() = logger::log_level::DEBUG;
 
     LOGD() << "hello, world!";
--- src/utils.h
+++ src/utils.h
@@ -2,4 +2,4 @@
 
 #define DELETE_COPY_AND_ASSIGN(class_name)              \
     class_name(const class_name&) = delete;             \
-    class_name& operator=(const class_name&) = delete;
+    class_name& operator=(const class_name&) = delete
