11eb408c1c313e99401913d03cde309f687229b2
ck <cengizkandemir@live.com>
2021-03-06 00:10:36 +0100

    Fix squared error calculation
    Pixel iterators are now properly dereferenced while calculating squared
    error sum.

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -55,7 +55,7 @@ float se(const monochrome_image& image1, const monochrome_image& image2)
     float sum = 0;
 
     for(const auto& [px1, px2]: make_image_zip(image1, image2)) {
-        const auto val = px1.value - px2.value;
+        const auto val = *px1.value - *px2.value;
         sum += val * val;
     }
 
